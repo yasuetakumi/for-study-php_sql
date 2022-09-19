8064f724f17d380f9fc16f213a4b802343c9202a
yasuetakumi <t-yasue@grune.co.jp>
2022-09-19 18:08:53 +0900

    herokuデータベース設定

--- library.php
+++ library.php
@@ -7,7 +7,7 @@ function h($value) {
 
 // dbに接続する
 function dbconnect() {
-  $db = new mysqli('localhost:3306', 'root', 'root', 'study_app');
+  $db = new mysqli('us-cdbr-east-06.cleardb.net', 'bff80db3a1e66b', '143d2e74', 'heroku_db42ef48675b9bd');
   if (!$db) {
     die($db->error);
   }

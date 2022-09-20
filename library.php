b56457e4eaa85a5421318dff0a948378f5777ed3
yasuetakumi <t-yasue@grune.co.jp>
2022-09-20 10:32:04 +0900

    一覧画面修正

--- library.php
+++ library.php
@@ -7,8 +7,8 @@ function h($value) {
 
 // dbに接続する
 function dbconnect() {
-  // $db = new mysqli('us-cdbr-east-06.cleardb.net', 'bff80db3a1e66b', '143d2e74', 'heroku_db42ef48675b9bd');
-  $db = new mysqli('localhost:3306', 'root', 'root', 'study_app');
+  $db = new mysqli('us-cdbr-east-06.cleardb.net', 'bff80db3a1e66b', '143d2e74', 'heroku_db42ef48675b9bd');
+  // $db = new mysqli('localhost:3306', 'root', 'root', 'study_app');
   if (!$db) {
     die($db->error);
   }
